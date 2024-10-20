50959f181d8826b91178cbfc4e13fb845b176eb0
Agricio_neto <nerovigiann@hotmail.com>
2024-10-20 13:19:24 -0400

    set port new email

--- api/mail_send.php
+++ api/mail_send.php
@@ -30,7 +30,7 @@ try {
     $mail->Username   = $_ENV['SERVER'];                     //SMTP username
     $mail->Password   = $_ENV['PASSWORD'];                              //SMTP password
     //$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
     $mail->client_email = $_ENV['SERVER'];
